058a9c3fb7fea553e830f109d470a44752cb9203
sXX1250245-hoshi <s1250245@u-aizu.ac.jp>
2017-07-06 19:30:39 +0900

    fix 04/address_book_demo

--- src/04_array_hash/address_book_demo.rb
+++ src/04_array_hash/address_book_demo.rb
@@ -1,2 +1,9 @@
 def address_book(book, names)
+  
+  (names.map do |name|
+     human = book[name]
+     children = human[:children].nil? ? 0 : human[:children]
+     [human[:name], children, human[:role]].join(", ")
+   end).join("\n")
+  
 end
